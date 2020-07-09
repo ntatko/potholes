bb515c4420432c527781d2f2fd1c93675a31700b
Noah Tatko <tatkon@rbtrainers.com>
2020-07-09 16:05:11 -0500

    initial commit

---
+++ src/components/UploadModal.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+import Dialog from '@material-ui/core/Dialog'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+
+import Input from '@material-ui/core/Input'
+
+class UploadModal extends Component {
+
+  render() {
+    return (
+      <>
+        <Dialog
+        >
+          <DialogTitle>Upload Pothole Image</DialogTitle>
+          <DialogContent>
+
+
+          </DialogContent>
+        </Dialog>
+      </>
+    )
+  }
+}
