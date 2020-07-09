092b1f68acbbb34650fe93e86bcf227ed370e1b3
Jake Webb <jakewebbsite@gmail.com>
2020-07-09 16:11:22 -0500

    center and zoom on holiday shores

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Map, Controls, LayerPanel, Popup, loadDataLayer } from '@bayer/ol-kit'
+import { Map, Controls, LayerPanel, Popup, centerAndZoom } from '@bayer/ol-kit'
 
 class App extends React.Component {
   onMapInit = async map => {
+    const opts = {
+      x: -89.938355,
+      y: 38.923748,
+      zoom: 14,
+    }
+    centerAndZoom(map, opts)
   }
 
   render () {
     return (
-      <Map fullScreen >
+      <Map
+        fullScreen
+        onMapInit={this.onMapInit}
+        updateUrlFromView={false}
+        updateViewFromUrl={false}>
         <Controls />
         <LayerPanel />
         <Popup />
